3e8db726f334f9909ac83883c5ad281e82c4c69a
EDCTor <tor@aimsparking.com>
2018-11-23 16:15:55 -0500

    Initial Commit: Basic Functionality

---
+++ GCV-Tester/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GCV_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtDirectoryPath = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(679, 446);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(133, 54);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtDirectoryPath
+            // 
+            this.txtDirectoryPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDirectoryPath.Enabled = false;
+            this.txtDirectoryPath.Location = new System.Drawing.Point(22, 12);
+            this.txtDirectoryPath.Name = "txtDirectoryPath";
+            this.txtDirectoryPath.Size = new System.Drawing.Size(694, 20);
+            this.txtDirectoryPath.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 397);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectDirectory.Location = new System.Drawing.Point(726, 10);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDirectory.TabIndex = 4;
+            this.btnSelectDirectory.Text = "Select Files";
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.btnSelectDirectory_Click);
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Location = new System.Drawing.Point(22, 39);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(64, 13);
+            this.lblFileCount.TabIndex = 5;
+            this.lblFileCount.Text = "0 files found";
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbResults.Location = new System.Drawing.Point(506, 74);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.Size = new System.Drawing.Size(294, 347);
+            this.rtbResults.TabIndex = 6;
+            this.rtbResults.Text = "";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Location = new System.Drawing.Point(506, 446);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(133, 54);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Previous";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 503);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.btnSelectDirectory);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtDirectoryPath);
+            this.Controls.Add(this.btnNext);
+            this.Name = "Form1";
+            this.Text = "GCV Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtDirectoryPath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ GCV-Tester/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+using Google.Cloud.Vision.V1;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf;
+
+namespace GCV_Tester
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap working_bm = null;
+        private Google.Cloud.Vision.V1.Image working_gbm = null;
+        private string[] files = null;
+        private int currentFile = -1;
+        private static readonly object refresh_lock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DisposeBitmaps()
+        {
+            if (this.working_bm != null) { this.working_bm.Dispose(); }
+        }
+
+        private void btnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && 
+                !string.IsNullOrWhiteSpace(fbd.SelectedPath) &&
+                Directory.Exists(fbd.SelectedPath)
+                )
+            {
+                this.files = Directory.GetFiles(fbd.SelectedPath);                
+                txtDirectoryPath.Text = fbd.SelectedPath;
+                lblFileCount.Text = this.files.Length.ToString() + " file(s) found";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string directory = System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath) + "\\Resources\\pictures\\";
+            if (Directory.Exists(directory))
+            {
+                txtDirectoryPath.Text = directory;
+                this.files = Directory.GetFiles(directory);
+                lblFileCount.Text = this.files.Length.ToString() + " file(s) found";
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            lock (refresh_lock)
+            {
+                if (this.files == null ||
+                this.files.Length == 0)
+                {
+                    // error cases, no files
+                    return;
+                }
+
+                if (this.files.Length == this.currentFile)
+                {
+                    // if we are at the last file, then reset to the first file
+                    this.currentFile = -1;
+                }
+
+                // increment the pointer to which file we are looking at
+                this.currentFile += 1;
+
+                // load the file
+                if (File.Exists(this.files[this.currentFile]))
+                {
+                    this.working_bm = new Bitmap(this.files[this.currentFile]);
+                    pictureBox1.Image = this.working_bm;
+
+                    // Load an image from a local file.
+                    this.working_gbm = Google.Cloud.Vision.V1.Image.FromFile(this.files[this.currentFile]);
+                    var client = ImageAnnotatorClient.Create();
+                    var response = client.DetectText(this.working_gbm);
+
+                    // keep track of debug info we are going to print out
+                    string s = string.Empty;
+
+                    foreach (var annotation in response)
+                    {
+                        if (annotation.BoundingPoly != null)
+                        {
+                            int w = annotation.BoundingPoly.Vertices[1].X - annotation.BoundingPoly.Vertices[0].X;
+                            int h = annotation.BoundingPoly.Vertices[2].Y - annotation.BoundingPoly.Vertices[1].Y;
+
+                            using (Graphics g = Graphics.FromImage(this.working_bm))
+                            {
+
+                                Pen p;
+                                if (((decimal)h / (decimal)w >= 0.2m) && ((decimal)h / (decimal)w <= 0.5m))
+                                {
+                                    p = new Pen(Color.Red, 5);
+
+                                    if (!string.IsNullOrEmpty(annotation.Description))
+                                    {
+                                        s = s + "Good Ratio h/w:" + ((decimal)h / (decimal)w).ToString() + System.Environment.NewLine;
+                                        s = s + annotation.Description + System.Environment.NewLine;
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Pen(Color.Blue, 5);
+
+                                    if (!string.IsNullOrEmpty(annotation.Description))
+                                    {
+                                        s = s + "Bad Ratio h/w:" + ((decimal)h / (decimal)w).ToString() + System.Environment.NewLine;
+                                        s = s + annotation.Description + System.Environment.NewLine;
+                                    }
+                                }
+                                
+                                g.DrawRectangle(p,
+                                    annotation.BoundingPoly.Vertices[0].X,
+                                    annotation.BoundingPoly.Vertices[0].Y,
+                                    w, h
+                                    );
+                                pictureBox1.Image = this.working_bm;
+                            }
+                        }
+                    }
+                    rtbResults.Text = s;
+                }
+
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            lock (refresh_lock)
+            {
+                if (this.files == null ||
+                this.files.Length == 0)
+                {
+                    // error cases, no files
+                    return;
+                }
+
+                if (this.currentFile < 0)
+                {
+                    // if we are at the first file, then reset to the last file
+                    this.currentFile = this.files.Length;
+                }
+
+                // increment the pointer to which file we are looking at
+                this.currentFile -= 1;
+
+                // load the file
+                if (File.Exists(this.files[this.currentFile]))
+                {
+                    this.working_bm = new Bitmap(this.files[this.currentFile]);
+                    pictureBox1.Image = this.working_bm;
+
+                    // Load an image from a local file.
+                    this.working_gbm = Google.Cloud.Vision.V1.Image.FromFile(this.files[this.currentFile]);
+                    var client = ImageAnnotatorClient.Create();
+                    var response = client.DetectText(this.working_gbm);
+
+                    // keep track of debug info we are going to print out
+                    string s = string.Empty;
+
+                    foreach (var annotation in response)
+                    {
+                        if (annotation.BoundingPoly != null)
+                        {
+                            int w = annotation.BoundingPoly.Vertices[1].X - annotation.BoundingPoly.Vertices[0].X;
+                            int h = annotation.BoundingPoly.Vertices[2].Y - annotation.BoundingPoly.Vertices[1].Y;
+
+                            using (Graphics g = Graphics.FromImage(this.working_bm))
+                            {
+
+                                Pen p;
+                                if (((decimal)h / (decimal)w >= 0.2m) && ((decimal)h / (decimal)w <= 0.5m))
+                                {
+                                    p = new Pen(Color.Red, 5);
+
+                                    if (!string.IsNullOrEmpty(annotation.Description))
+                                    {
+                                        s = s + "Good Ratio h/w:" + ((decimal)h / (decimal)w).ToString() + System.Environment.NewLine;
+                                        s = s + annotation.Description + System.Environment.NewLine;
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Pen(Color.Blue, 5);
+
+                                    if (!string.IsNullOrEmpty(annotation.Description))
+                                    {
+                                        s = s + "Bad Ratio h/w:" + ((decimal)h / (decimal)w).ToString() + System.Environment.NewLine;
+                                        s = s + annotation.Description + System.Environment.NewLine;
+                                    }
+                                }
+
+                                g.DrawRectangle(p,
+                                    annotation.BoundingPoly.Vertices[0].X,
+                                    annotation.BoundingPoly.Vertices[0].Y,
+                                    w, h
+                                    );
+                                pictureBox1.Image = this.working_bm;
+                            }
+                        }
+                    }
+                    rtbResults.Text = s;
+                }
+
+            }
+        }
+    }
+}
